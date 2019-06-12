8fd742563267cd56a08e4421dba44c6c18bec898
imfaazi <faazfajib7@gmail.com>
2019-06-12 22:15:17 +0530

    meteor and crater

---
+++ Planet/Assets/Scripts/Meteor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Meteor : MonoBehaviour
+{
+    public GameObject craterPrefab;
+   
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.collider.name == "Planet")
+        {
+            Quaternion rot = Quaternion.LookRotation(transform.position.normalized);
+            rot *= Quaternion.Euler(90f, 0f, 0f);
+            Instantiate(craterPrefab, col.contacts[0].point, rot);
+            Destroy(this.gameObject);
+        }
+    }
+}
