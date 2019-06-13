bb59cbbba26767836eb5c6d8c839433680483564
imfaazi <faazfajib7@gmail.com>
2019-06-13 19:10:08 +0530

    score

--- Planet/Assets/Scripts/PlayerCollision.cs
+++ Planet/Assets/Scripts/PlayerCollision.cs
@@ -1,18 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerCollision : MonoBehaviour
 {
-  
+    public Text scoreText;
+    int x = 0;
+    public static bool isDead = false;
+
+    private void Start()
+    {
+        StartCoroutine(ScoreIncrementer());
+    }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.collider.tag == "CraterGroup") 
         {
-           // Debug.Log(collision.collider.name);
+            // Debug.Log(collision.collider.name);
+
+            isDead = true;
             Destroy(this.gameObject);
+            
         }
     }
 
-    
+    IEnumerator ScoreIncrementer()
+    {
+        if(isDead == false)
+        {
+        x += 10;
+        scoreText.text = x.ToString();
+        yield return new WaitForSeconds(0.3f);
+        StartCoroutine(ScoreIncrementer());
+        }
+    }
+
 }
