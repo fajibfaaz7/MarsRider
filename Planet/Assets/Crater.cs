de296670f6609559c598001fdce1913737b5c85e
imfaazi <faazfajib7@gmail.com>
2019-06-13 14:36:16 +0530

    player collision

---
+++ Planet/Assets/Crater.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crater : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Destroy(this.gameObject, 7f);
+    }
+}
---
+++ Planet/Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+  
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "CraterGroup") 
+        {
+           // Debug.Log(collision.collider.name);
+            Destroy(this.gameObject);
+        }
+    }
+
+    
+}
