926b17ebb605056dc3f43774110663157382b2a5
imfaazi <faazfajib7@gmail.com>
2019-06-14 19:41:41 +0530

    player crash

--- Planet/Assets/Scripts/PlayerCollision.cs
+++ Planet/Assets/Scripts/PlayerCollision.cs
@@ -6,14 +6,21 @@ using UnityEngine.UI;
 public class PlayerCollision : MonoBehaviour
 {
     public static bool isDead = false;
+    public GameObject playerDeathPrefab;
+    public GameObject playerDeathEffectPrefab;
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.collider.tag == "CraterGroup")
         {
             // Debug.Log(collision.collider.name);
-
+            Quaternion rot = Quaternion.LookRotation(transform.position.normalized);
+            rot *= Quaternion.Euler(90f, 0f, 0f);
+            Vector3 pos = collision.contacts[0].point;
             isDead = true;
             Destroy(this.gameObject);
+            Instantiate(playerDeathPrefab,pos,rot);
+            Instantiate(playerDeathEffectPrefab, pos, rot);
+            
 
         }
     }
