c9f9e19235e7f6bf1b2106a8979c7907fcff9c07
imfaazi <faazfajib7@gmail.com>
2019-06-15 18:22:16 +0530

    settings

---
+++ Planet/Assets/Settings.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Settings : MonoBehaviour
+{
+    public GameObject mainmenuUI;
+    public GameObject settingsUI;
+    public Text highScoreText;
+    void Start()
+    {
+        mainmenuUI.SetActive(true);
+        settingsUI.SetActive(false);
+        highScoreText.text = "0";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        highScoreText.text = PlayerPrefs.GetInt("HighScore", 0).ToString();
+    }
+
+
+    public void SettingsOpen()
+    {
+        mainmenuUI.SetActive(false);
+        settingsUI.SetActive(true);
+    }
+
+    public void SettingsClose()
+    {
+        settingsUI.SetActive(false);
+        mainmenuUI.SetActive(true);
+    }
+
+    public void DeleteHighScore()
+    {
+        PlayerPrefs.DeleteKey("HighScore");
+    }
+}
