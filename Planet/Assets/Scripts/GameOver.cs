67898e524e11ccfac8e6cbfff3e518a3accc207e
imfaazi <faazfajib7@gmail.com>
2019-06-18 13:09:48 +0530

    revive and optimization

--- Planet/Assets/Scripts/GameOver.cs
+++ Planet/Assets/Scripts/GameOver.cs
@@ -5,11 +5,14 @@ public class GameOver : MonoBehaviour
 {
     public GameObject gameOverUI;
     public Text highScoreUI;
+    public GameObject reviveButton;
+    public static int count;
 
     void Start()
     {
         highScoreUI.text = PlayerPrefs.GetInt("HighScore", 0).ToString();
         gameOverUI.SetActive(false);
+        
     }
 
     
@@ -20,5 +23,34 @@ public class GameOver : MonoBehaviour
             gameOverUI.SetActive(true);
             highScoreUI.text = PlayerPrefs.GetInt("HighScore", 0).ToString();
         }
+        if (count == 1)
+        {
+            reviveButton.SetActive(false);
+        }
+        if (count == 0)
+        {
+            reviveButton.SetActive(true);
+        }
+        
+    }
+
+
+    public void OnceMore()
+    {
+        count = 1;
+        GameManager.instance.Revive();
+
+    }
+
+    public void NoMore()
+    {
+        count = 0;
+        GameManager.instance.Restart();
+    }
+
+    public void MainMenu()
+    {
+        count = 0;
+        GameManager.instance.MainMenu();
     }
 }
